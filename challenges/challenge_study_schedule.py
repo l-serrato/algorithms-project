c0b88484b95472aee4d94b5d1b9b59883d7f7810
Luis <l-serrato@live.com>
2023-07-06 15:28:36 -0300

    reqs1 e 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    if target_time is None:
+        return None
+    for each_period in permanence_period:
+        if type(each_period[0]) != int or type(each_period[1]) != int:
+            return None
+        if each_period[0] <= target_time <= each_period[1]:
+            counter += 1
+    return counter
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,74 +1,22 @@
 from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    # Test case 1: Valid input with odd key
-    message = "Hello, World!"
-    key = 3
-    expected_output = "lroW_,olleH"
-    assert encrypt_message(message, key) == expected_output, "Test case 1 fail"
+    """ a"""
 
-    # Test case 2: Valid input with even key
-    message = "Hello, World!"
-    key = 6
-    expected_output = "Wo,_lleHlrd"
-    assert encrypt_message(message, key) == expected_output, "Test case 2 fail"
+    # Even key
+    assert encrypt_message("message", 4) == "ega_ssem"
 
-    # Test case 3: Invalid key type (not an integer)
-    message = "Hello, World!"
-    key = "invalid"
-    try:
-        encrypt_message(message, key)
-        assert False, "Test case 3 failed"
-    except TypeError:
-        pass
+    # Odd key
+    assert encrypt_message("message", 3) == "sem_egas"
 
-    # Test case 4: Invalid message type (not a string)
-    message = 12345
-    key = 3
-    try:
-        encrypt_message(message, key)
-        assert False, "Test case 4 failed"
-    except TypeError:
-        pass
+    # Invalid key
+    assert encrypt_message("message", -2) == "egassem"
 
-    # Test case 5: Invalid key (out of range)
-    message = "Hello, World!"
-    key = 100
-    expected_output = "!dlroW ,olleH"
-    assert encrypt_message(message, key) == expected_output, "Test case 5 fail"
+    # Incorrect input type
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(5, 3)
 
-    # Test case 6: Empty message with odd key
-    message = ""
-    key = 5
-    expected_output = ""
-    assert encrypt_message(message, key) == expected_output, "Test case 6 fail"
-
-    # Test case 7: Empty message with even key
-    message = ""
-    key = 4
-    expected_output = ""
-    assert encrypt_message(message, key) == expected_output, "Test case 7 fail"
-
-    # Test case 8: Invalid implementation (no type validation)
-    def encrypt_message_no_type_validation(message, key):
-        if key not in range(1, len(message)):
-            return "".join(reversed(message))
-
-        part_one = reversed(message[:key])
-        part_two = reversed(message[key:])
-
-        if not key % 2:
-            part_two, part_one = part_one, part_two
-
-        return "".join(part_one) + "_" + "".join(part_two)
-
-    message = "Hello, World!"
-    key = 3
-    try:
-        encrypt_message_no_type_validation(message, key)
-        assert False, "Test case 8 failed"
-    except TypeError:
-        pass
-
-    print("All test cases passed!")
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("trybe", 3.9)
