71b7626ea115826f51d1744f543a3b3123f384d4
Luis <l-serrato@live.com>
2023-07-06 14:49:17 -0300

    req 2?

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,23 +3,19 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 def test_encrypt_message():
     # Test case 1: Valid input with odd key
-    message = "Hello"
+    message = "Hello, World!"
     key = 3
-    expected_output = "lroHe"
-    assert (
-        encrypt_message(message, key) == expected_output
-    ), "Test case 1 failed"
+    expected_output = "lroW_,olleH"
+    assert encrypt_message(message, key) == expected_output, "Test case 1 fail"
 
     # Test case 2: Valid input with even key
-    message = "Hello"
-    key = 2
-    expected_output = "olleH"
-    assert (
-        encrypt_message(message, key) == expected_output
-    ), "Test case 2 failed"
+    message = "Hello, World!"
+    key = 6
+    expected_output = "Wo,_lleHlrd"
+    assert encrypt_message(message, key) == expected_output, "Test case 2 fail"
 
     # Test case 3: Invalid key type (not an integer)
-    message = "Hello"
+    message = "Hello, World!"
     key = "invalid"
     try:
         encrypt_message(message, key)
@@ -27,8 +23,31 @@ def test_encrypt_message():
     except TypeError:
         pass
 
+    # Test case 4: Invalid message type (not a string)
+    message = 12345
+    key = 3
+    try:
+        encrypt_message(message, key)
+        assert False, "Test case 4 failed"
+    except TypeError:
+        pass
+
+    # Test case 5: Invalid key (out of range)
+    message = "Hello, World!"
+    key = 100
+    expected_output = "!dlroW ,olleH"
+    assert encrypt_message(message, key) == expected_output, "Test case 5 fail"
+
+    # Test case 6: Empty message with odd key
+    message = ""
+    key = 5
+    expected_output = ""
+    assert encrypt_message(message, key) == expected_output, "Test case 6 fail"
+
+    # Test case 7: Empty message with even key
+    message = ""
+    key = 4
+    expected_output = ""
+    assert encrypt_message(message, key) == expected_output, "Test case 7 fail"
+
     print("All test cases passed!")
-
-
-# Run the test function
-test_encrypt_message()
