7efb1a11b7ed963395170b75446376f0d415a0ab
Luis <l-serrato@live.com>
2023-07-06 14:36:36 -0300

    req4?

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,38 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+
+    sorted_first = sort_string(first_string)
+    sorted_second = sort_string(second_string)
+
+    return sorted_first, sorted_second, sorted_first == sorted_second
+
+
+def sort_string(string):
+    if len(string) <= 1:
+        return string
+
+    mid = len(string) // 2
+    left = sort_string(string[:mid])
+    right = sort_string(string[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return "".join(merged)
+
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,34 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    # Test case 1: Valid input with odd key
+    message = "Hello"
+    key = 3
+    expected_output = "lroHe"
+    assert (
+        encrypt_message(message, key) == expected_output
+    ), "Test case 1 failed"
+
+    # Test case 2: Valid input with even key
+    message = "Hello"
+    key = 2
+    expected_output = "olleH"
+    assert (
+        encrypt_message(message, key) == expected_output
+    ), "Test case 2 failed"
+
+    # Test case 3: Invalid key type (not an integer)
+    message = "Hello"
+    key = "invalid"
+    try:
+        encrypt_message(message, key)
+        assert False, "Test case 3 failed"
+    except TypeError:
+        pass
+
+    print("All test cases passed!")
+
+
+# Run the test function
+test_encrypt_message()
